0f17b7c197ed9654275157a6f03a933f21a10b79
test_endless@126.com <test_endless@126.com>
2022-05-26 11:44:42 +0800

    android 开发

---
+++ app/src/main/java/cn/dreamchase/android/eleven/MainActivity.java
@@ -0,0 +1,17 @@
+package cn.dreamchase.android.eleven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * -百度定位SDK
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
